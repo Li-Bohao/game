6b1a74fb77b1a0f93517848c021c9b036d10ada3
Li Bohao <372815053@qq.com>
2026-04-14 23:30:09 +0800

    刚知道，昨天的不是多例模式，气笑了

--- include/multiton.h
+++ include/multiton.h
@@ -1,74 +1,41 @@
 #ifndef MULTITON_H
 #define MULTITON_H
-#include <memory>
 #include <utility>
 #include <stdexcept>
 #include <format>
 #include <typeinfo>
-#include <mutex>
-class MultitonOverLimit : public std::logic_error
-{
-public:
-    explicit MultitonOverLimit(const std::string &msg) : std::logic_error(msg) {}
-};
 class MultitonOutOfRange : public std::logic_error
 {
 public:
     explicit MultitonOutOfRange(const std::string &msg) : std::logic_error(msg) {}
 };
-class MultitonDoubleRelease : public std::logic_error
-{
-public:
-    explicit MultitonDoubleRelease(const std::string &msg) : std::logic_error(msg) {}
-};
 template <typename T, size_t max>
-requires (max != 0)
+    requires(max != 0)
 class Multiton
 {
 private:
-    inline static std::unique_ptr<T> m_ptr[max]{};
-    inline static std::mutex m_mutex;
+    inline static T m_instances[max]{};
 
 protected:
     Multiton() = default;
 
 public:
     Multiton(const Multiton &) = delete;
+
     Multiton &operator=(const Multiton &) = delete;
     Multiton(Multiton &&) = delete;
     Multiton &operator=(Multiton &&) = delete;
-    template <typename... Args>
-    static std::pair<size_t order, T &> NewInstance(Args &&...args)
+
+    static T &GetInstance(size_t order)
     {
-        std::lock_guard lock(m_mutex);
-        for (size_t i = 0; i < max; ++i)
+        if (order >= max)
         {
-            if (nullptr == m_ptr[i])
-            {
-                m_ptr[i] = std::make_unique<T>(std::forward<Args>(args)...);
-                return {i + 1, *m_ptr[i]};
-            }
+            throw MultitonOutOfRange(std::format("Attempting to get an object \
+                at index {} from class {} exceeds the index limit of {}.",
+                                                 order, typeid(T).name(), max));
         }
-        throw MultitonOverLimit(std::format("Class {} attempts \
-            to construct an instance that exceeds the instance count limit of {}.",
-                                            typeid(T).name(), max));
-    }
-    static void DestroyInstance(size_t order)
-    {
-        std::lock_guard lock(m_mutex);
-        if (order > max || 0 == order)
-        {
-            throw MultitonOutOfRange(std::format("Class {} attempts \
-                to release an instance with index {}, which is out of range {}.",
-                                                 typeid(T).name(), order, max));
-        }
-        if (nullptr == m_ptr[order - 1])
-        {
-            throw MultitonDoubleRelease(std::format("Class {} attempts \
-                to release an instance with index {} twice.",
-                                                 typeid(T).name(), order));
-        }
-        m_ptr[order - 1].reset();
+
+        return m_instances[order];
     }
 };
 #endif
