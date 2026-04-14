e4cb1321f2a0afa54b4ab64ca8e23e732be8344f
Li Bohao <372815053@qq.com>
2026-04-15 00:02:26 +0800

    把日志声明和实现放一起了，以后其他的估计也会。我是不是闲的？

--- include/log.h
+++ include/log.h
@@ -1,15 +1,40 @@
-#pragma once
-#include<string>
-#include<fstream>
-#include<mutex>
-constexpr const char* log_file_name = "LOG.txt";
-class Log {
+#ifndef LOG_H
+#define LOG_H
+#include <string>
+#include <fstream>
+#include <mutex>
+#include <chrono>
+#include <format>
+constexpr const char *log_file_name = "LOG.txt";
+class Log
+{
 private:
 	std::mutex m_mutex;
-	std::string get_time();
-	std::ofstream& get_file();
-	void record(std::string_view level, std::string_view content);
+	std::string get_time()
+	{
+		auto now = std::chrono::system_clock::now();
+		auto local_time = std::chrono::zoned_time{std::chrono::current_zone(), now};
+		return std::format("{:%Y-%m-%d %H:%M:%S}", local_time);
+	}
+	std::ofstream &get_file()
+	{
+		static std::ofstream log_file(log_file_name, std::ios::app);
+		return log_file;
+	}
+	void record(std::string_view level, std::string_view content)
+	{
+		std::lock_guard(m_mutex);
+		get_file() << get_time() << ' ' << level << ": " << content << std::endl;
+	}
+
 public:
-	void Error(std::string_view content);
-	void Notice(std::string_view content);
+	void Error(std::string_view content)
+	{
+		record("Error", content);
+	}
+	void Notice(std::string_view content)
+	{
+		record("Notice", content);
+	}
 };
+#endif
