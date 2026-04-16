fc0e98ff528b4190114cb858b02b8f247db1e428
Li Bohao <372815053@qq.com>
2026-04-17 00:50:23 +0800

    到底还是把引用符号给忘了，麻

--- include/utils.h
+++ include/utils.h
@@ -4,20 +4,20 @@ class NonCopyable
 {
 public:
     NonCopyable(const NonCopyable &) = delete;
-    NonCopyable operator=(const NonCopyable &) = delete;
+    NonCopyable &operator=(const NonCopyable &) = delete;
 };
 class NonMovable
 {
 public:
     NonMovable(NonMovable &&) = delete;
-    NonMovable operator=(NonMovable &&) = delete;
+    NonMovable &operator=(NonMovable &&) = delete;
 };
 class NonTransferable
 {
 public:
     NonTransferable(const NonTransferable &) = delete;
-    NonTransferable operator=(const NonTransferable &) = delete;
+    NonTransferable &operator=(const NonTransferable &) = delete;
     NonTransferable(NonTransferable &&) = delete;
-    NonTransferable operator=(NonTransferable &&) = delete;
+    NonTransferable &operator=(NonTransferable &&) = delete;
 };
 #endif
