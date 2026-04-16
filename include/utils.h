ec20c7ebe2044837bc2dc738699608281751c33a
Li Bohao <372815053@qq.com>
2026-04-17 01:09:28 +0800

    奇怪，不把NonTransferable的默认构造显式声明protected的话子类Multiton的构造是被删除的

--- include/utils.h
+++ include/utils.h
@@ -2,18 +2,27 @@
 #define UTILS_H
 class NonCopyable
 {
+protected:
+    NonCopyable() = default;
+
 public:
     NonCopyable(const NonCopyable &) = delete;
     NonCopyable &operator=(const NonCopyable &) = delete;
 };
 class NonMovable
 {
+protected:
+    NonMovable() = default;
+
 public:
     NonMovable(NonMovable &&) = delete;
     NonMovable &operator=(NonMovable &&) = delete;
 };
 class NonTransferable
 {
+protected:
+    NonTransferable() = default;
+
 public:
     NonTransferable(const NonTransferable &) = delete;
     NonTransferable &operator=(const NonTransferable &) = delete;
